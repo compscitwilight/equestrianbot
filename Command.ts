b5a5615c2d758b8c7afe802eb09e64b0abedbc04
devrusty <rguillory246@gmail.com>
2023-01-03 02:08:19 -0600

    added autocmplete

--- Command.ts
+++ Command.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder, Interaction } from "discord.js";
 export default interface Command {
     data: SlashCommandBuilder,
+    autocomplete: (interaction: Interaction) => any,
     execute: (interaction: Interaction) => any
 }
