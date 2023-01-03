94688f9a15ba1af1bd778ee3d8d299eed3b50495
devrusty <rguillory246@gmail.com>
2023-01-03 08:57:32 -0600

    swapped for filtered

--- commands/PonyInfo.ts
+++ commands/PonyInfo.ts
@@ -17,10 +17,11 @@ export default {
         const choices = Ponies.map((pony) => {
             return pony.name;
         });
-        choices.length = 25; // cap
         const filtered = choices.filter((choice) => {
             return choice.toLowerCase().includes(val.value.toLowerCase());
         });
+
+        filtered.length = 25; // cap
         await interaction.respond(filtered.map((choice) => ({name: choice, value: choice})));
     },
     execute: (interaction: Interaction) => {
