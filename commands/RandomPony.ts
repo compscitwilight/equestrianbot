f10114092e735a4dfbc7d409a7ac1ea31467fb68
devrusty <rguillory246@gmail.com>
2023-01-04 04:14:50 -0600

    removed button

--- commands/RandomPony.ts
+++ commands/RandomPony.ts
@@ -3,35 +3,19 @@ import { CreatePonyEmbed } from "../embeds/Pony";
 import { Ponies } from "../data/ponies.data";
 import Command from "../Command";
 
-function sendRandomPony(interaction: Interaction) {
-    if (!interaction.isRepliable()) return;
-    let index = Math.floor(Math.random() * Ponies.length);
-    let pony = Ponies[index];
-    let embed = CreatePonyEmbed(pony);
-
-    let repeatButton = new ButtonBuilder({
-        customId: "primary",
-        label: "Get another",
-        style: ButtonStyle.Success
-    })
-    let row = new ActionRowBuilder<ButtonBuilder>({components: [repeatButton]})
-    let collector = interaction.channel.createMessageComponentCollector({time: 15000});
-    collector.on("collect", () => {
-        sendRandomPony(interaction);
-    })
-
-    interaction.reply({
-        content: "🎲 RandomPony!",
-        embeds: [embed],
-        components: [row]
-    });
-}
-
 export default {
     data: new SlashCommandBuilder()
         .setName("randompony")
         .setDescription("Sends a random pony with a brief description, image, and "),
     execute: async (interaction: Interaction) => {
-        sendRandomPony(interaction);
+        if (!interaction.isRepliable()) return;
+        let index = Math.floor(Math.random() * Ponies.length);
+        let pony = Ponies[index];
+        let embed = CreatePonyEmbed(pony);
+    
+        interaction.reply({
+            content: "🎲 RandomPony!",
+            embeds: [embed]
+        });
     }
 } as Command;
