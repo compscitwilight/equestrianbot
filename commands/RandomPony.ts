8c11b32c130b7d219802a33c28f3276889c4e850
devrusty <rguillory246@gmail.com>
2023-01-04 04:09:59 -0600

    added button

--- commands/RandomPony.ts
+++ commands/RandomPony.ts
@@ -1,21 +1,36 @@
-import { SlashCommandBuilder, Interaction } from "discord.js";
+import { SlashCommandBuilder, Interaction, ActionRowBuilder, ButtonBuilder, ButtonStyle,  } from "discord.js";
 import { CreatePonyEmbed } from "../embeds/Pony";
 import { Ponies } from "../data/ponies.data";
 import Command from "../Command";
+
+function sendRandomPony(interaction: Interaction) {
+    if (!interaction.isRepliable()) return;
+    let index = Math.floor(Math.random() * Ponies.length);
+    let pony = Ponies[index];
+    let embed = CreatePonyEmbed(pony);
+
+    interaction.reply({
+        content: "🎲 RandomPony!",
+        embeds: [embed]
+    });
+}
 
 export default {
     data: new SlashCommandBuilder()
         .setName("randompony")
         .setDescription("Sends a random pony with a brief description, image, and "),
     execute: async (interaction: Interaction) => {
-        if (!interaction.isRepliable()) return;
-        let index = Math.floor(Math.random() * Ponies.length);
-        let pony = Ponies[index];
-        let embed = CreatePonyEmbed(pony);
+        let repeatButton = new ButtonBuilder({
+            customId: "primary",
+            label: "Get another",
+            style: ButtonStyle.Success
+        })
+        let row = new ActionRowBuilder({components: [repeatButton]})
+        let collector = interaction.channel.createMessageComponentCollector({time: 15000});
+        collector.on("collect", () => {
+            sendRandomPony(interaction);
+        })
 
-        interaction.reply({
-            content: "🎲 RandomPony!",
-            embeds: [embed]
-        });
+        sendRandomPony(interaction);
     }
 } as Command;
