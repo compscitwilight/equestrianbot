211df3db4f9ade3c42b5e65d03f9328fe185b61f
devrusty <rguillory246@gmail.com>
2023-01-04 06:15:40 -0600

    added get another btton

--- commands/RandomPony.ts
+++ commands/RandomPony.ts
@@ -1,7 +1,14 @@
-import { SlashCommandBuilder, Interaction, ActionRowBuilder, ButtonBuilder, ButtonStyle,  } from "discord.js";
+import { SlashCommandBuilder, Interaction, ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 import { CreatePonyEmbed } from "../embeds/Pony";
 import { Ponies } from "../data/ponies.data";
 import Command from "../Command";
+
+function getRandomPonyEmbed() {
+    let index = Math.floor(Math.random() * Ponies.length);
+    let pony = Ponies[index];
+    let embed = CreatePonyEmbed(pony);
+    return embed;
+}
 
 export default {
     data: new SlashCommandBuilder()
@@ -9,13 +16,29 @@ export default {
         .setDescription("Sends a random pony with a brief description, image, and "),
     execute: async (interaction: Interaction) => {
         if (!interaction.isRepliable()) return;
-        let index = Math.floor(Math.random() * Ponies.length);
-        let pony = Ponies[index];
-        let embed = CreatePonyEmbed(pony);
-    
-        interaction.reply({
+        let repeatButton = new ButtonBuilder({
+            customId: "primary",
+            label: "Get another",
+            style: ButtonStyle.Success
+        });
+        let row = new ActionRowBuilder<ButtonBuilder>({
+            components: [repeatButton]
+        });
+        let collector = interaction.channel.createMessageComponentCollector();
+        collector.on("collect", async () => {
+            let embed = getRandomPonyEmbed();
+            await interaction.followUp({
+                content: "🎲 RandomPony!",
+                embeds: [embed],
+                components: [row]
+            })
+        })
+
+        let embed = getRandomPonyEmbed();
+        await interaction.reply({
             content: "🎲 RandomPony!",
-            embeds: [embed]
+            embeds: [embed],
+            components: [row]
         });
     }
 } as Command;
