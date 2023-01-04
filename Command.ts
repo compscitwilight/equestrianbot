5b7734eda9018ad1b1d1b9495a76d81579925fbf
devrusty <rguillory246@gmail.com>
2023-01-04 02:02:01 -0600

    added ponylist command

--- Command.ts
+++ Command.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder, Interaction, AutocompleteInteraction, SlashCommandStringOption } from "discord.js";
 export default interface Command {
     data: SlashCommandBuilder,
-    autocomplete: (interaction: AutocompleteInteraction) => any,
+    autocomplete?: (interaction: AutocompleteInteraction) => any,
     execute: (interaction: Interaction) => any
 }
---
+++ commands/PonyList.ts
@@ -0,0 +1,21 @@
+import Command from "../Command";
+import { Interaction, SlashCommandBuilder } from "discord.js";
+import { Ponies } from "../data/ponies.data";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("ponylist")
+        .setDescription("Returns a list of all of the characters.")
+        .setDMPermission(true),
+    execute: async (interaction: Interaction) => {
+        if (!interaction.isRepliable()) return;
+        interaction.reply("I've sent a list to your DMs.");
+
+        let list = Ponies.map((pony) => {
+            return `* ${pony.name}`;
+        })
+        await interaction.user.dmChannel.send({
+            content: list.toString()
+        })
+    }
+} as Command
