5921941680acd8b4921d796e27d1233e2f2634cd
devrusty <rguillory246@gmail.com>
2023-01-06 16:52:26 -0600

    using links insterad

--- utils/sendDm.ts
+++ utils/sendDm.ts
@@ -1,21 +1,11 @@
-import { Interaction, MessageCreateOptions } from "discord.js";
+import { EmbedBuilder, Interaction, MessageCreateOptions } from "discord.js";
 export async function sendDm(interaction: Interaction, msg: MessageCreateOptions) {
     if (!interaction.isRepliable()) return;
     let dmChannel = await interaction.user.createDM(true);
 
     function cannotDmResponse() {
         return {
-            content: "Sorry, but I cannot DM you as your DMs seem to be private",
-            files: [
-                {
-                    attachment: "./assets/enabling_dms_1.png",
-                    name: "1"
-                },
-                {
-                    attachment: "./assets/enabling_dms_2.png",
-                    name: "2"
-                }
-            ],
+            content: "Sorry, but I cannot DM you as your DMs seem to be private\nhttps://cdn.discordapp.com/attachments/1041019407318663241/1061054600767090840/enabling_dms_1.png\nhttps://cdn.discordapp.com/attachments/1041019407318663241/1061054601169747988/enabling_dms_2.png",
             ephemeral: true
         }
     }
