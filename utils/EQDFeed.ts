5ce5d8b94566c4d6f4dfc5245274c5b9faafc0d5
devrusty <rguillory246@gmail.com>
2023-01-08 13:48:36 -0600

    created getFormattedFeed

--- utils/EQDFeed.ts
+++ utils/EQDFeed.ts
@@ -1,1 +1,8 @@
-import * as axios from "axios";
+import axios from "axios";
+
+let feed = "https://www.equestriadaily.com/feeds/posts/default";
+
+export async function getFormattedFeed() {
+    let response = await axios.get(feed);
+    return response;
+}
