a6d204963ea4b11a75f5ad1256e69bdfe2e72dce
devrusty <rguillory246@gmail.com>
2023-01-08 18:20:49 -0600

    moved to disabled

---
+++ commands/disabled/EquestriaDaily.ts
@@ -0,0 +1,14 @@
+import { Interaction, SlashCommandBuilder } from "discord.js";
+import Command from "../../Command";
+import { getFormattedFeed } from "../../utils/EQDFeed";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("equestriadaily")
+        .setDescription("Sends the latest EquestriaDaily post."),
+    execute: async (interaction: Interaction) => {
+        if (!interaction.isRepliable()) return;
+        let feed = await getFormattedFeed();
+        interaction.reply("WIP");
+    }
+} as Command
