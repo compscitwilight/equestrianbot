6c6b355d647451d647ae9a937cc934a157136c42
devrusty <rguillory246@gmail.com>
2023-01-08 18:07:10 -0600

    some stuff for future me to work on

--- utils/EQDFeed.ts
+++ utils/EQDFeed.ts
@@ -4,7 +4,9 @@ let feed = "https://www.equestriadaily.com/feeds/posts/default";
 
 export async function getFormattedFeed() {
     let response = await axios.get(feed);
-    let data = response.data;
-    console.log(typeof data);
+    let data = response.data as string;
+    let start = data.split("<entry>")[1];
+    let cutOffs = start.replace("</entry>", " ").split(/ g/);
+    console.log(cutOffs);
     return data;
 }
