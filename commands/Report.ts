4ac01ef89e50e10e087a81dadb105348cfc7d5a4
devrusty <rguillory246@gmail.com>
2023-01-08 22:30:47 -0600

    did stuff

--- commands/Report.ts
+++ commands/Report.ts
@@ -5,7 +5,8 @@ import {
     SlashCommandBuilder,
     TextInputStyle,
     Interaction,
-    TextInputBuilder
+    TextInputBuilder,
+    StringSelectMenuBuilder
 } from "discord.js";
 
 export default {
@@ -23,21 +24,36 @@ export default {
 		const subject = new TextInputBuilder({
             customId: "subject",
             label: "Subject",
+            placeholder: "The subject of your report",
             style: TextInputStyle.Short,
             required: true
+        });
+
+        const reportType = new StringSelectMenuBuilder({
+            customId: "type",
+            placeholder: "Report Type",
+            options: [
+                { label: "Bug / Issue", description: "Any type of bug or issue in the bot.", value: "bug" },
+                { label: "Feature Suggestion", description: "Any kind of feature that you would like to see implemented.", value: "feature" },
+                { label: "Character Edit", description: "If you find an issue in any of the character embeds, then use this option.", value: "character" },
+                { label: "Other", description: "Any other type of report that hasn't been specified.", value: "other" }
+            ]
         });
 
 		const description = new TextInputBuilder({
             customId: "description",
             label: "Description",
+            placeholder: "Description of your report.",
             style: TextInputStyle.Paragraph,
             required: true
         });
 
 		const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(subject);
-		const secondActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(description);
+		const secondActionRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(reportType);
+        const thirdActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(description);
 
-		modal.addComponents(firstActionRow, secondActionRow);
+		modal.addComponents(firstActionRow, thirdActionRow);
+        //modal.addComponents(secondActionRow);
 		await interaction.showModal(modal);
     }
 } as Command;
